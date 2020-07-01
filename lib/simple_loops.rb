d7f47de72020dcd0ea28df6d8575e9075d5666fa
Mimi Ojserkis <mimioj@gmail.com>
2020-07-01 22:30:59 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,13 +1,19 @@
 def loop_message_five_times(string)
-  
   5.times do
     puts string
   end
-  
 end
+
 
 def loop_message_n_times(string, n)
   n.times do
     puts string
   end
+end
+
+
+def output_array(arr)
+
+
+
 end
